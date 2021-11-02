5a3ced23df8f1ce428abe71292b89e99d18c4af9
Snir David Nahari <snir-david@users.noreply.github.com>
2021-11-02 21:26:26 +0200

    start ex1

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+# Snir David Nahari 205686538
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def reshape(image_fname, centroids_fname):
+    centroids = np.loadtxt(centroids_fname)  # load centroids
+    orig_pixels = plt.imread(image_fname)
+    pixels = orig_pixels.astype(float) / 255
+    # Reshape the image(128x128x3) into an Nx3 matrix where N = number of pixels.
+    return pixels.reshape(-1, 3), centroids
+
+# def find_closest_centroids(pixel, centroids):
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def k_means(pixels_list, centroids_list, output_file):
+    print(pixels_list)
+    print(centroids_list)
+    # for i in range(19):
+    #     for pixel in pixels_list:
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    pixels, centroids = reshape(sys.argv[1], sys.argv[2])
+    k_means(pixels, centroids, sys.argv[3])
