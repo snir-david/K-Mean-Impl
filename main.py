27a415cb29e0f2dbe0cbb27842f49f10a9252bd0
Snir David Nahari <snir-david@users.noreply.github.com>
2021-11-03 21:31:41 +0200

    progress, bug with percision

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Snir David Nahari 205686538
 import sys
 import numpy as np
-from numpy import inf
+from numpy import inf, sqrt
 import matplotlib.pyplot as plt
 
 
@@ -13,11 +13,27 @@ def reshape(image_fname, centroids_fname):
     return pixels.reshape(-1, 3), centroids
 
 
+def find_new_centroids(pixel_to_centroids_list, old_cent):
+    new_centroids = []
+    for i in range(len(pixel_to_centroids_list)):
+        sum_pixels = sum(pixel_to_centroids_list[i])
+        num_of_pixels = len(pixel_to_centroids_list[i])
+        if num_of_pixels > 0:
+            new_cent = sum_pixels / num_of_pixels
+            new_centroids.append(new_cent.round(4)
+                                 )
+        else:
+            new_centroids.append(old_cent[i])
+
+    return new_centroids
+
+
 def find_closest_centroids(pixel, centroids):
     min_dist = inf
     centroid_id = -1
     for centroid in centroids:
-        distance = abs(pixel[0] - centroid[0]) + abs(pixel[1] - centroid[1]) + abs(pixel[2] - centroid[2])
+        distance = sqrt(
+            pow(pixel[0] - centroid[0], 2) + pow(pixel[1] - centroid[1], 2) + pow(pixel[2] - centroid[2], 2))
         if distance < min_dist:
             min_dist = distance
             centroid_id = np.where(centroids == centroid)
@@ -26,12 +42,24 @@ def find_closest_centroids(pixel, centroids):
 
 def k_means(pixels_list, centroids_list, output_file):
     pixels_to_centroids = []
+    coverage = 0
+    old_cent = centroids_list
     for i in range(len(centroids_list)):
         pixels_to_centroids.append([])
-    for i in range(19):
-        for pixel in pixels_list:
-            id = find_closest_centroids(pixel, centroids_list)
-            pixels_to_centroids[id].append(pixel)
+    for i in range(20):
+        if coverage < 2:
+            for pixel in pixels_list:
+                id = find_closest_centroids(pixel, centroids_list)
+                pixels_to_centroids[id].append(pixel)
+            new_cent = find_new_centroids(pixels_to_centroids, centroids_list)
+            print("iter: ")
+            print(i)
+            print(new_cent)
+            compare = old_cent == new_cent
+            if compare.all():
+                coverage += 1
+            else:
+                centroids_list = new_cent
 
 
 if __name__ == '__main__':
